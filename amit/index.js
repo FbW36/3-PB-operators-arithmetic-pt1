b27d809c7fd25d7d3bcf9b517a2c5ec229717a54
Amit833 <amitmajumder833@gmail.com>
2020-10-05 15:35:08 +0200

    3rd exercise

---
+++ amit/index.js
@@ -0,0 +1,30 @@
+let snackServings = 22;
+let guests = 25;
+let add = snackServings + guests;
+let sub = snackServings - guests;
+let mal = snackServings * guests;
+let div = snackServings / guests;
+let drinks = 10;
+let resultOne = mal / drinks;
+console.log(resultOne);
+
+let monkeys = 15;
+let bananas = 9;
+let div2 = monkeys / bananas;
+
+let monkeyfights = 20;
+let add2 = monkeys + bananas;
+let mal2 = add2 * monkeyfights;
+let resultTwo = mal2;
+console.log(resultTwo);
+
+monkeys = monkeys + 10;
+bananas = bananas - 5;
+let sub2 = bananas - monkeys;
+let happiness = sub2;
+let add3 = happiness + monkeyfights;
+let resultThree = add3;
+console.log(add3);
+
+let remainder = resultOne % resultTwo;
+console.log(remainder);
