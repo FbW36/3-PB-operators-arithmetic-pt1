492fa597cbefe2941f77f871f5ec117ea147d92e
Tomas <eliz.tomas23@gmail.com>
2020-10-05 14:34:15 +0200

    results printed

---
+++ Tomas/index.js
@@ -0,0 +1,31 @@
+let snackServings = 4
+let guests = 8
+console.log(snackServings, guests)
+
+console.log(snackServings - guests)
+console.log(guests -snackServings)
+console.log(snackServings * guests)
+console.log(snackServings / guests)
+
+let drinks = 10
+let resultOne = snackServings * guests / drinks
+console.log(resultOne)
+
+let monkeys = 15
+let bananas = 9
+console.log(monkeys / bananas)
+
+let monkeyFights = 20
+let resultTwo = (monkeys + bananas) * monkeyFights
+console.log(resultTwo)
+
+monkeys += 1
+console.log(monkeys)
+
+bananas -= 1
+console.log(bananas)
+
+let happiness = bananas - monkeys
+console.log(happiness + monkeyFights)
+
+console.log(resultOne / resultTwo)
