f8ded329a018622b42aa56731e5dead96d2fe673
Luke Dowling <lpdowling26@googlemail.com>
2020-10-05 14:11:39 +0200

    finished assignment

---
+++ luke/index.js
@@ -0,0 +1,41 @@
+// task 1
+let snackServings = 26;
+let guests = 9;
+
+// task 2
+console.log(snackServings - guests);
+console.log(guests - snackServings);
+
+// task 3
+console.log(snackServings * guests);
+
+// task 4
+console.log(snackServings / guests);
+
+// task 5
+let drinks = 10;
+let resultOne = (snackServings * guests) / drinks;
+console.log(resultOne);
+
+// task 5.2
+let monkeys = 15;
+let bananas = 9;
+console.log(monkeys / bananas);
+
+// task 6
+let monkeyFights = 20;
+let resultTwo = (monkeys + bananas) * monkeyFights;
+console.log(resultTwo);
+
+// task 7
+monkeys = 20;
+
+// task 8
+bananas = 2;
+
+// task 9
+let happiness = bananas - monkeys;
+console.log(monkeyFights + happiness);
+
+// task 10
+console.log(resultOne % resultTwo);
