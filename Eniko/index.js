802d4277aedd31db17637cec773f589e4ffe0b4d
Enikő Veszelka <encsooo@gmail.com>
2020-10-05 14:42:38 +0200

    done, but unsure about 8-11

---
+++ Eniko/index.js
@@ -0,0 +1,31 @@
+var snackServings = 120;
+var guests = 150;
+
+console.log(snackServings - guests);
+console.log(guests - snackServings);
+
+console.log(snackServings / guests);
+console.log(snackServings * guests);
+
+var drinks = 10;
+console.log((snackServings * guests) / drinks);
+let resultOne = ((snackServings * guests) / drinks);
+
+var monkeys = 15;
+var bananas = 9;
+var monkeyFights = 20;
+console.log(monkeys / bananas);
+console.log(monkeys + bananas / monkeyFights);
+let resultTwo = (monkeys + bananas / monkeyFights);
+
+monkeys = ++ monkeys;
+bananas = -- bananas;
+console.log(monkeys);
+
+let happiness = (monkeys - bananas);
+console.log(happiness);
+console.log(happiness + monkeyFights);
+
+console.log(resultOne % resultTwo);
+
+
