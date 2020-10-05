00329d42e19f656942e1172cddc374bd8e777f05
Joyce <puch1996@gmail.com>
2020-10-05 15:55:59 +0200

    modified

---
+++ Joyce/index.js
@@ -0,0 +1,57 @@
+// 0
+let snackServings = 6;
+let guests = 24;
+
+// 1
+let add = snackServings + guests;
+console.log(add);
+
+// 2
+let sub1 = snackServings - guests;
+console.log(sub1);
+let sub2 = guests - snackServings;
+console.log(sub2);
+
+// 3
+let mul = snackServings * guests;
+console.log(mul);
+
+// 4
+let div = snackServings / guests;
+console.log(div);
+
+// 5
+let drinks = 10;
+// let mul = snackServings * guests;
+let resultOne = mul / drinks;
+console.log(resultOne);
+// 144
+
+// 6
+let monkeys = 15;
+let bananas = 9;
+let divM = monkeys / bananas;
+console.log(divM);
+
+// 7
+let monkeyFights = 20;
+let addM = monkeys + bananas;
+let resultTwo = addM * monkeyFights;
+console.log(resultTwo);
+
+// 8 Increment monkeys
+monkeys = monkeys + 1;
+console.log(monkeys);
+
+// 9 Decrement bananas
+monkeys = monkeys - 1;
+console.log(monkeys);
+
+// 10
+let happiness = bananas - monkeys;
+let addH = happiness + monkeyFights;
+console.log(addH);
+
+// 11
+let resultFinal = resultOne / resultTwo;
+console.log(resultFinal);
