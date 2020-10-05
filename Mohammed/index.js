00080b966a9b987c6fd91d84abb3e33fcb7aebcb
mashraee <m.almashraee@gmail.com>
2020-10-05 15:14:22 +0200

    exercise No.3

---
+++ Mohammed/index.js
@@ -0,0 +1,53 @@
+// 0
+let snackServings = 20;
+let guests = 10;
+console.log(snackServings, guests);
+
+// 1
+let sum = snackServings + guests;
+console.log(sum);
+
+// 2
+let sub1 = snackServings - guests;
+let sub2 = guests - snackServings;
+console.log(sub1, sub2);
+
+// 3
+let mul = snackServings * guests;
+console.log(mul);
+
+// 4
+let div = snackServings / guests;
+console.log(div);
+
+// 5
+let drinks = 10;
+let resultOne = mul /drinks;
+console.log(resultOne);
+
+// 6
+let monkeys = 15;
+let bananas = 9;
+div = monkeys / bananas;
+console.log(div);
+
+// 7
+let monkeyfights = 20;
+sum = monkeys + bananas;
+let resultTwo = sum * monkeyfights;
+console.log(resultTwo);
+
+// 8
+console.log(monkeys +=1);
+
+// 9
+console.log(bananas -=1);
+
+// 10
+let happiness = bananas - monkeys;
+sum = happiness + monkeyfights;
+console.log(sum);
+
+// 11
+let rem = resultOne % resultTwo;
+console.log(resultOne, resultTwo, 'The remainder is :' , rem);
