d31df09762fb66455c165e79cfd023bfda4b0d23
Zarzarius <zarzarius@gmail.com>
2020-10-06 00:24:53 +0200

    exercise done

---
+++ Azael/index.js
@@ -0,0 +1,61 @@
+// 0. Declare two variables "snackServings" and "guests" and assign them number values.
+
+var snackServings = 66
+var guests = 12
+
+// 1. Add snackServings and guests.
+
+var add = snackServings + guests
+
+// 2. Subtract guests from snackServings. Then, subtract snackServings from guests.
+
+var sub = snackServings - guests
+
+// 3. Multiply snackServings and guests.
+
+var mal = snackServings * guests
+
+// 4. Divide snackServings by guests.
+
+var div = snackServings / guests
+
+// 5. Declare another variable "drinks" with value "10". Multiply snackServings and guests. Then, divide the result by drinks. Store the result in a new variable named "resultOne". Print "resultOne" to the console.
+
+var drinks = 10
+var mal2 = snackServings * guests
+var resultOne = mal2 / drinks
+console.log(resultOne)
+
+// 5. Declare two variables: "monkeys" with value 15, and "bananas" with value 9. Divide monkeys by bananas.
+
+var monkeys = 15
+var bananas = 9
+var div2 = monkeys / bananas
+
+// 6. Declare another variable "monkeyfights" with value 20. Add monkeys and bananas, then multiply the result by monkeyfights. Store the result in variable "resultTwo".  Print "resultTwo" to the console.
+
+var monkeyfights = 20
+var add2 = monkeys + bananas
+resultTwo = add2 * monkeyfights
+console.log(resultTwo)
+
+// 7. Increment monkeys.
+
+monkeys = ++monkeys
+
+
+
+// 8. Decrement bananas.
+
+bananas = --bananas
+
+
+// 9. Subtract monkeys from bananas and store this in a new variable "happiness". Add happiness and monkeyfights.
+var happiness = bananas - monkeys
+
+var add3 = happiness + monkeyfights
+
+// 10. Print the remainder when "resultOne" is divided by "resultTwo" to the console.
+
+var module = resultOne % resultTwo
+console.log(module)
