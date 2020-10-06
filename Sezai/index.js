9eb92213e3e83b6240959a9714b018c55440f565
Sezai Keskin <sezai.keskin@me.com>
2020-10-06 14:25:16 +0200

    reworked it - thanks cia:)

--- Sezai/index.js
+++ Sezai/index.js
@@ -20,8 +20,8 @@ resultOne = (snackServings * guests) / drinks;
 console.log(resultOne);
 
 //5b.
-let monkeys = 20;
-let bananas = 5;
+let monkeys = 15;
+let bananas = 9;
 console.log(monkeys / bananas);
 
 // 6.
@@ -29,7 +29,9 @@ let monkeyfights = 20;
 let resultTwo = monkeys + bananas * monkeyfights;
 console.log(resultTwo);
 
-// 7. & 8. see at 5b.
+// 7. & 8. see at 5b. REVIEWED Thanks CIAMAC
+console.log(++monkeys);
+console.log(--bananas);
 
 // 9.
 let happiness = monkeys - bananas;
