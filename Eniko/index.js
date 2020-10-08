639bc97594b56fe8d5cd80acef0a008e6c4e0c7f
Enikő Veszelka <encsooo@gmail.com>
2020-10-08 15:12:26 +0200

    korrigiert

--- Eniko/index.js
+++ Eniko/index.js
@@ -1,6 +1,6 @@
 var snackServings = 120;
 var guests = 150;
-
+console.log(snackServing + guests);
 console.log(snackServings - guests);
 console.log(guests - snackServings);
 
@@ -15,12 +15,15 @@ var monkeys = 15;
 var bananas = 9;
 var monkeyFights = 20;
 console.log(monkeys / bananas);
-console.log(monkeys + bananas / monkeyFights);
-let resultTwo = (monkeys + bananas / monkeyFights);
+console.log((monkeys + bananas) / monkeyFights);
+let resultTwo = ((monkeys + bananas) / monkeyFights);
 
-monkeys = ++ monkeys;
-bananas = -- bananas;
+monkeys += 1;
+//bananas = -- bananas;
+//bananas --;
+--bananas;
 console.log(monkeys);
+console.log(bananas);
 
 let happiness = (monkeys - bananas);
 console.log(happiness);
